129a0f8c70e8667dbe2a4672a979c2c2ef87b946
Prince Sharma <princeky420@gmail.com>
2021-07-03 11:24:57 +0530

    first commit

---
+++ admin/layout/about-modal.php
@@ -0,0 +1,9 @@
+<div id="modal1" class="modal">
+    <div class="modal-content">
+      <h4>About TechBar</h4> <!--change-->
+      <p>We are TechBar. One Resturant powered by the skilled chef only! We love to make delicious foods and serve our customers. We have been doing this for many years now!</p> <!--change-->
+    </div>
+    <div class="modal-footer">
+      <a href="#!" class="modal-close waves-effect waves-green btn-flat">Close</a>
+    </div>
+  </div>
